4266e99b52ce3bf087429ff61df7441ece8d6f62
SamSepiolM <117809309+SamSepiolM@users.noreply.github.com>
2024-11-09 17:02:28 -0600

    UploadFiles

---
+++ PixelFilm/frmHistograma.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace PixelFilm
+{
+    partial class frmHistograma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Draw_RGB = new System.Windows.Forms.CheckBox();
+            this.Draw_R = new System.Windows.Forms.CheckBox();
+            this.Draw_G = new System.Windows.Forms.CheckBox();
+            this.Draw_B = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Draw_RGB
+            // 
+            this.Draw_RGB.AutoSize = true;
+            this.Draw_RGB.BackColor = System.Drawing.Color.White;
+            this.Draw_RGB.Location = new System.Drawing.Point(27, 286);
+            this.Draw_RGB.Name = "Draw_RGB";
+            this.Draw_RGB.Size = new System.Drawing.Size(100, 21);
+            this.Draw_RGB.TabIndex = 0;
+            this.Draw_RGB.Text = "Draw_RGB";
+            this.Draw_RGB.UseVisualStyleBackColor = false;
+            this.Draw_RGB.CheckedChanged += new System.EventHandler(this.Draw_RGB_CheckedChanged);
+            // 
+            // Draw_R
+            // 
+            this.Draw_R.AutoSize = true;
+            this.Draw_R.BackColor = System.Drawing.Color.White;
+            this.Draw_R.Location = new System.Drawing.Point(156, 286);
+            this.Draw_R.Name = "Draw_R";
+            this.Draw_R.Size = new System.Drawing.Size(100, 21);
+            this.Draw_R.TabIndex = 1;
+            this.Draw_R.Text = "Draw_R     ";
+            this.Draw_R.UseVisualStyleBackColor = false;
+            this.Draw_R.CheckedChanged += new System.EventHandler(this.Draw_R_CheckedChanged);
+            // 
+            // Draw_G
+            // 
+            this.Draw_G.AutoSize = true;
+            this.Draw_G.BackColor = System.Drawing.Color.White;
+            this.Draw_G.Location = new System.Drawing.Point(27, 323);
+            this.Draw_G.Name = "Draw_G";
+            this.Draw_G.Size = new System.Drawing.Size(101, 21);
+            this.Draw_G.TabIndex = 2;
+            this.Draw_G.Text = "Draw_G     ";
+            this.Draw_G.UseVisualStyleBackColor = false;
+            this.Draw_G.CheckedChanged += new System.EventHandler(this.Draw_G_CheckedChanged);
+            // 
+            // Draw_B
+            // 
+            this.Draw_B.AutoSize = true;
+            this.Draw_B.BackColor = System.Drawing.Color.White;
+            this.Draw_B.Location = new System.Drawing.Point(156, 323);
+            this.Draw_B.Name = "Draw_B";
+            this.Draw_B.Size = new System.Drawing.Size(99, 21);
+            this.Draw_B.TabIndex = 3;
+            this.Draw_B.Text = "Draw_B     ";
+            this.Draw_B.UseVisualStyleBackColor = false;
+            this.Draw_B.CheckedChanged += new System.EventHandler(this.Draw_B_CheckedChanged);
+            // 
+            // frmHistograma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.ClientSize = new System.Drawing.Size(282, 353);
+            this.Controls.Add(this.Draw_B);
+            this.Controls.Add(this.Draw_G);
+            this.Controls.Add(this.Draw_R);
+            this.Controls.Add(this.Draw_RGB);
+            this.Name = "frmHistograma";
+            this.Text = "frmHistograma";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmHistograma_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox Draw_RGB;
+        private System.Windows.Forms.CheckBox Draw_R;
+        private System.Windows.Forms.CheckBox Draw_G;
+        private System.Windows.Forms.CheckBox Draw_B;
+    }
+}
---
+++ PixelFilm/frmVideos.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace PixelFilm
+{
+    public partial class frmVideos : Form
+    {
+
+        private Filtros filtros = new Filtros();
+
+        private Bitmap picOriginal;
+        private Bitmap picResultado;
+
+        frmHistograma histo = new frmHistograma();
+        private bool vHistograma = false;
+
+        private VideoCapture capture;
+
+        private Timer timer;
+
+        private int filtroSeleccionado = -1, resolucion = 4;
+
+        private string rutaVideo;
+
+        private bool play = false;
+
+        private float ondas = 0;
+
+        bool ondasA = false;
+
+        float nuevox = 0;
+
+        private int valorTrackBar = 5;
+
+        OpenCvSharp.Size sizeVideo = new OpenCvSharp.Size();
+
+        Bitmap vhsOriginal, vhs;
+
+        public frmVideos()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frmVideos_Load(object sender, EventArgs e)
+        {
+            //axWindowsMediaPlayer1.Visible = true;
+            //axWindowsMediaPlayer1.URL = "C:/Users/L/Videos/PIA_PI/PIA PI/PixelFilm/Resources/Video_Prueba.mp4";
+            //axWindowsMediaPlayer1.Ctlcontrols.play();
+            picOriginal = (Bitmap)pictureBox2.Image;
+            picResultado = picOriginal;
+
+            vhsOriginal = new Bitmap(Path.GetFullPath("..\\..\\Resources\\VCR-Overlay.png"));
+            vhs = vhsOriginal;
+
+            trackBar1.Value = valorTrackBar;
+
+
+            cambiarResolucion(resolucion);
+
+            comboBox1.Items.Add("Original");
+            comboBox1.Items.Add("1/2");
+            comboBox1.Items.Add("1/3");
+            comboBox1.Items.Add("1/4");
+            comboBox1.Items.Add("1/5");
+            comboBox1.SelectedIndex = 3;
+
+            listBox1.Items.Add("Filtro Negativo");
+            listBox1.Items.Add("Filtro Escala de grises");
+            listBox1.Items.Add("Filtro Correccion de Gamma");
+            listBox1.Items.Add("Filtro Blur Gaussiano");
+            listBox1.Items.Add("Filtro Enfoque Laplaciano");
+            listBox1.Items.Add("Filtro Mejorar Brillo y Contraste");
+            listBox1.Items.Add("Filtro Umbral");
+
+            listBox1.Items.Add("Filtro Interferencia VHS");
+            listBox1.Items.Add("Filtro Ruido Aleatorio");
+            listBox1.Items.Add("Filtro Deformacion de Ondas");
+            listBox1.Items.Add("Filtro Ensombrecer");
+        }
+
+        private void iconGuardar_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                int itemIndex = listBox1.SelectedIndex;
+                string itemString = listBox1.SelectedItem.ToString();
+
+                filtroSeleccionado = itemIndex;
+                if (!play)
+                {
+                    AplicarFiltros();
+                }
+
+                listBox2.Items.Clear();
+                listBox2.Items.Add(itemString);
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            picResultado = picOriginal;
+            //picResultado = new Bitmap(picOriginal, sizeVideo.Width, sizeVideo.Height);
+            pictureBox2.Image = picOriginal;
+            listBox2.Items.Clear();
+            filtroSeleccionado = -1;
+
+            ActualizarHisto();
+        }
+
+        private void iconEliminar_Click(object sender, EventArgs e)
+        {
+            rutaVideo = "..\\..\\Resources\\Video_Prueba.mp4";
+            cargarVideo(rutaVideo);
+        }
+
+        public void cargarVideo(string ruta)
+        {
+            Desconectar();
+            ruta = Path.GetFullPath(ruta);
+            rutaVideo = ruta;
+
+            capture = new VideoCapture(ruta);
+
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("No puedo Martha");
+                return;
+            }
+
+            // Maneja el evento Tick del temporizador para actualizar el PictureBox con los fotogramas
+            timer = new Timer();
+            timer.Interval = (int)capture.Fps / 2;
+            //timer.Interval = 2; 
+            Reproducir();
+        }
+
+        public void Desconectar()
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+            }
+
+            if (capture != null)
+            {
+                capture.Release();
+                capture.Dispose();
+            }
+
+            filtroSeleccionado = -1;
+            play = false;
+            listBox2.Items.Clear();
+        }
+
+        private void iconPlay_Click(object sender, EventArgs e)
+        {
+            Reproducir();
+        }
+
+        private void iconPause_Click(object sender, EventArgs e)
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+                play = false;
+            }
+        }
+
+        private void iconStop_Click(object sender, EventArgs e)
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+                play = false;
+            }
+            
+            if (capture != null)
+            {
+                capture.Release();
+            }
+        }
+
+        private void Reproducir()
+        {
+            if (capture != null && play == false)
+            {
+                timer.Start();
+                timer.Tick += (sender1, e1) =>
+                {
+                    try
+                    {
+                        Mat frame = new Mat();
+                        
+                        capture.Read(frame);
+                        
+                        if (frame.Empty())
+                        {
+                            //OpenCvSharp.Size size = new OpenCvSharp.Size(pictureBox2.Width, pictureBox2.Height);
+                            //frame.Resize(size);
+                            play = false;
+                            timer.Stop();
+                            capture.PosFrames = 0;
+                            
+                            capture.Release();
+
+                            cargarVideo(rutaVideo);
+                            return;
+                        }
+
+                        //float aspect = (float)frame.Width / (float)frame.Height;
+
+                        //OpenCvSharp.Size sizeVideo2 = new OpenCvSharp.Size(aspect * sizeVideo.Width, sizeVideo.Height);
+                        picOriginal = BitmapConverter.ToBitmap(frame.Resize(sizeVideo));
+                        //picOriginal = BitmapConverter.ToBitmap(frame.Resize(sizeVideo2));
+                        picResultado = picOriginal;
+
+                        AplicarFiltros();
+
+                        play = true;
+                    }
+                    catch
+                    {
+                        return;
+                    }
+                };
+            }
+        }
+
+        private void AplicarFiltros()
+        {
+            //AplicaFiltros
+            switch (filtroSeleccionado)
+            {
+                case 0:
+                    {
+                        picResultado = filtros.filtroNegativo(picResultado);
+                        break;
+                    }
+                case 1:
+                    {
+                        picResultado = filtros.filtroEscalaGrises(picResultado);
+                        break;
+                    }
+                case 2:
+                    {
+                        picResultado = filtros.filtroCorreccionGamma(picResultado, (trackBar1.Value / 5.0f), (trackBar2.Value / 5.0f), (trackBar3.Value / 5.0f));
+                        break;
+                    }
+                case 3:
+                    {
+                        picResultado = filtros.filtroBlurGaussiano(picResultado, trackBar1.Value);
+                        break;
+                    }
+                case 4:
+                    {
+                        picResultado = filtros.filtroEnfoqueLaplaciano(picResultado, (int)(trackBar1.Value * 3 + 1));
+                        break;
+                    }
+                case 5:
+                    {
+                        picResultado = filtros.filtroBrilloYContraste(picResultado, (float)(trackBar1.Value / 5.0f), (float)(trackBar2.Value / 5.0f) * 2.5f);
+                        break;
+                    }
+                case 6:
+                    {
+                        picResultado = filtros.filtroUmbral(picResultado, (int)(trackBar1.Value * 25.5f));
+                        break;
+                    }
+
+                case 7:
+                    {
+                        nuevox += (float)(trackBar1.Value / 5.0f + 0.4f) * 0.15f;
+                        //picResultado = filtros.filtroInterferenciaVHS1(picResultado, nuevox);
+                        picResultado = new Bitmap(picOriginal, sizeVideo.Width, sizeVideo.Height);
+                        picResultado = filtros.filtroInterferenciaVHS2(picResultado, vhs, nuevox, (int)((trackBar2.Value /8.0f) * 8.0f), trackBar3.Value * 7);
+                        break;
+                    }
+                case 8:
+                    {
+                        picResultado = filtros.filtroRuidoAleatorio2(picResultado, (int)(trackBar1.Value));
+                        break;
+                    }
+                case 9:
+                    {
+                        if (ondas >= 4)
+                        {
+                            ondasA = true;
+                        }
+                        if (ondas <= -4)
+                        {
+                            ondasA = false;
+                        }
+                        if (ondasA)
+                        {
+                            ondas -= 0.4f;
+                        }
+                        else
+                        {
+                            ondas += 0.4f;
+                        }
+
+                        nuevox += (float)(trackBar1.Value/10.0f + 0.1f) * 0.3f;
+
+                        picResultado = filtros.filtroDeformacionOndas(picResultado, nuevox, -nuevox);
+                        break;
+                    }
+                case 10:
+                    {
+                        picResultado = filtros.filtroEnsombrecer_Iluminar(picResultado, (float)(trackBar1.Value/5.0f + 0.4f) * 2.2f, (float)(trackBar2.Value / 10.0f));
+                        break;
+                    }
+
+                default:
+                    {
+                        break;
+                    }
+            }
+
+            pictureBox2.Image = picResultado;
+            ActualizarHisto();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int auxResolucion = comboBox1.SelectedIndex;
+
+            resolucion = auxResolucion + 1;
+            /*switch (auxResolucion)
+            {
+                case 0:
+                    {
+                        resolucion = 1;
+                        break;
+                    }
+            }*/
+            cambiarResolucion(resolucion);
+        }
+
+        private void btnVerHistograma_Click(object sender, EventArgs e)
+        {
+            cHistograma.Controls.Clear();
+            vHistograma = !vHistograma;
+
+            if (vHistograma)
+            {
+                AbrirFormulario(histo);
+                ActualizarHisto();
+            }
+        }
+
+        private void cambiarResolucion(int resize)
+        {
+            sizeVideo.Width = pictureBox2.Width / resize;
+            sizeVideo.Height = pictureBox2.Height / resize;
+
+            vhs = new Bitmap(vhsOriginal, sizeVideo.Width, sizeVideo.Height);
+           // picResultado = new Bitmap(picOriginal, sizeVideo.Width, sizeVideo.Height);
+        }
+
+        private void AbrirFormulario(Form formulario)
+        {
+            formulario.TopLevel = false;
+            formulario.FormBorderStyle = FormBorderStyle.None;
+            formulario.Dock = DockStyle.Fill;
+            //formulario.BackColor = Color.Firebrick;
+
+            cHistograma.Controls.Clear();
+            cHistograma.Controls.Add(formulario);
+            formulario.Show();
+        }
+
+        private void ActualizarHisto()
+        {
+            if (vHistograma)
+            {
+                histo.ActualizarHistograma(picResultado);
+                histo.Refresh();
+            }
+        }
+    }
+}
